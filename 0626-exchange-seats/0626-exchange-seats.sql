eeb5bd0148760c2167b92fa822a332143467e920
Anushkaa Prabhat <133905609+AnushkaaPrabhat15@users.noreply.github.com>
2024-08-20 03:56:46 -0700

    Time: 347 ms (40.20%), Space: 0B (100.00%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,7 +1,14 @@
-SELECT IF (id < (SELECT MAX(id) FROM Seat), 
-            IF(id % 2 = 0, id - 1, id + 1), 
-            IF(id % 2 = 0, id - 1, id)
-        ) AS id, student
-FROM Seat
-ORDER BY id;
+SELECT 
+CASE 
+WHEN id = (SELECT MAX(id) FROM seat) AND id % 2 = 1 THEN id 
+WHEN id % 2 = 1 THEN id + 1 ELSE id - 1 END AS id, student
+FROM seat
+ORDER BY id
 
+
+#to swap odd number and even number, use id % 2 to check
+#id % 2 = 1 is odd number. Then, add 1 to the id
+#id % 2 = 0 is even number. Then, subtract 1 from the id
+#but last odd number is not swapped (e.g. id=5 in example)
+#therefore we must check whether the id is max or not, by using subquery SELECT MAX(id) FROM seat
+#to sort by ID in the end
